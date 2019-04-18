4a89917ae6f14734ed126fd248fd5e90018fef4b
Roy PC <roysantos112@gmail.com>
2019-04-17 22:36:24 -0700

    update to version5

--- main.js
+++ main.js
@@ -2,7 +2,17 @@ var todoList = {
   todos: [],
 
   displayTodos: function() {
-    console.log(this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log("(x)", this.todos[i].todoText);
+        } else {
+          console.log("( )", this.todos[i].todoText);
+        }
+      }
+    }
   },
 
   addTodo: function(todoText) {
