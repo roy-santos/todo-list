241445c27a62191b766b19eba2148b03c54520c9
Roy PC <roysantos112@gmail.com>
2019-04-20 10:41:59 -0700

    update to version7

--- main.js
+++ main.js
@@ -51,15 +51,27 @@ var todoList = {
     //Case 1: checks everything is true, make it false
     if (completedTodos === totalTodos) {
       //Make everything false
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
+      for (var j = 0; j < totalTodos; j++) {
+        this.todos[j].completed = false;
       }
     } else {
       //Case 2: Otherwise, make everything true.
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
+      for (var k = 0; k < totalTodos; k++) {
+        this.todos[k].completed = true;
       }
     }
     this.displayTodos();
   }
 };
+
+//Get access to the buttons
+var displayTodosButton = document.getElementById("displayTodosButton");
+var toggleAllButton = document.getElementById("toggleAllButton");
+
+//Run the method when the button is clicked
+displayTodosButton.addEventListener("click", () => {
+  todoList.displayTodos();
+});
+toggleAllButton.addEventListener("click", () => {
+  todoList.toggleAll();
+});
